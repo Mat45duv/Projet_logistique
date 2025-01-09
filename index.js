7f912036ec74b136088fb234df7c3c722114f8f1
papo1243 <159899245+papo1243@users.noreply.github.com>
2025-01-09 09:28:16 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+/**
+ * @file index.js
+ * @author Maxencexz
+ * @description Fichier en charge de démarrer le serveur Express avec HTTPS.
+ */
+
+// On charge les variables d'environnement
+require("dotenv").config();
+
+// On importe les modules requis
+const __modules = {
+    express: require("express"),
+    expressBodyParser: require('body-parser'),
+    path: require("path"),
+    middlewares: require("./src/express/middlewares/middlewares"),
+    endpointsLoader: require("./src/express/endpointsLoader"),
+    nocache: require("nocache"),
+    https: require("https"), // HTTPS module
+    fs: require("fs") // File system module to read certificate and key files
+};
+
+// Création de l'application Express
+const app = __modules.express();
+app.disable("x-powered-by");
+app.set("view engine", "ejs");
+app.set('view cache', false);
+app.set('trust proxy', 1);
+app.use(__modules.express.static(__dirname + '/src/public'));
+app.set('views', __modules.path.join(__dirname, '/src/views'));
+app.use(__modules.expressBodyParser.json());
+app.use(__modules.expressBodyParser.urlencoded({ extended: true }));
+app.use(__modules.nocache());
+
+// Add middlewares
+app.use(__modules.middlewares.sessionMiddleware);
+app.use(__modules.middlewares.isSessionMiddleware);
+
+// Load HTTPS certificates
+const httpsOptions = {
+    key: __modules.fs.readFileSync(__dirname + '/certificate/localhost+2-key.pem'),
+    cert: __modules.fs.readFileSync(__dirname + '/certificate/localhost+2.pem')
+};
+
+// Start the HTTPS server
+const server = __modules.https.createServer(httpsOptions, app).listen(process.env.EXPRESS_PORT, async () => {
+    console.log(`[🔒] [WEB SERVER] Application Express démarrée en HTTPS; chargement des endpoints...`);
+    await __modules.endpointsLoader(app);
+});
